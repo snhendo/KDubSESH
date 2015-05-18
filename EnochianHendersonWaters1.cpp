246ca991375901a5dc982ecb4af311997dd5cbd8
snhendo <snhllama74@sbcglobal.net>
2015-05-18 17:07:11 -0400

    starting stuff

--- EnochianHendersonWaters1.cpp
+++ EnochianHendersonWaters1.cpp
@@ -1,5 +1,70 @@
 /*
 
- stuff
+Author: Stephen Enochian, Shannon Henderson, Kaitlyn Waters
+Course Title: Data Structures
+Course Number: CS2028
+Instructor: Anca Ralescu
+TA: Suryadip Chakraborty
+Abstract:
+Preconditions:
+Postcondition:
+Credit:
 
 */
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main () {
+    int shape = 0;
+    while (shape = 0) {
+        cout << "What shape?" << endl
+             << "0 = Triangle" << endl
+             << "1 = Quadrilateral" << endl
+             << "2 = Pentagon" << endl
+             << "3 = 
+    }
+}
+
+class Polygon {
+    virtual float area() = 0;
+    virtual float perimeter() = 0;
+}
+
+class Triangle : public Polygon {
+    virtual area
+}
+
+class IsoscelesTriangle : public Triangle {
+    
+}
+
+class EquilateralTriangle : public Triangle {
+    
+}
+
+class Quadrilateral : public Polygon {
+    
+}
+
+class Rectangle : public Quadrilateral {
+    
+}
+
+class Square : public Quadrilateral {
+    
+}
+
+class Pentagon : public Polygon {
+    
+}
+
+class Hexagon : public Polygon {
+    
+}
+
+class Octagon : public Polygon {
+    
+}

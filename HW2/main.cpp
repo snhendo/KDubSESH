23d58f6528bf752f05a894ed459b1fc9864130db
K Waters <waterskm@mail.uc.edu>
2015-05-28 13:00:29 -0400

    put in stuff from the word doc
    gonna need to edit it later

--- HW2/main.cpp
+++ HW2/main.cpp
@@ -18,6 +18,46 @@ using namespace std;
 
 int main(){
 
+bool Algorithm isUnique1(A, first, last){
+   /*Input: Array A, first, last
+   Output:
+true if the array contains no repeated elements
+false if the array contains repeated elements*/
+   if (first >= last)
+return true;
+    if (!isUnique1(A, first, last-1) {return false};
+    if (!isUnique1(A, first+1, last) {return false};
+    return(A[first]!=A[last])
+}
+
+
+bool Algorithm isUnique2(A, first, last){
+   /*Input: Array A, first, last
+   Output:
+true if the array contains no repeated elements
+false if the array contains repeated elements*/
+    if (first >= last) {return true;}
+    for (int i =  first; i < last; i++) {
+        for (int j = i+1; j <=last; j++) {
+            if (A[i] == A[j]) {return false};
+        }
+    }
+   return true;
+}
+
+bool Algorithm isUnique3(A, first, last){
+   /*Input: Array A, first, last
+   Output:
+true if the array contains no repeated elements
+false if the array contains repeated elements*/
+    if (first >= last) {return true;}
+   SORT(A, first, last); // lol wut
+    for (int i =  first; i < last; i++) {
+        if (A[i] == A[i+1]) {return false};
+    }
+   return true;
+}
+
 return 0;
 
 }

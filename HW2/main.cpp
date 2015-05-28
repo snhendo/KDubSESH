5801f0cc9637224c50f9ee1c585e899f2b3cccae
K Waters <waterskm@mail.uc.edu>
2015-05-28 12:50:31 -0400

    added folder and saved cpp file in it question mark

---
+++ HW2/main.cpp
@@ -0,0 +1,22 @@
+/* 20CS2028Assignment2-2015
+Authors: Stephen Enochian, Shannon Henderson, Kaitlyn Waters
+Course: Data Structures
+Course Number: CS2028
+Instructor: Anca Ralescu
+TA: Suryadip Chakraborty
+Abstract: Assignment 2 main.cpp uses three algorithms (Algorithm isUnique1 (A, first, last), Algorithm isUnique2 (A, first, last), and Algorithm isUnique3 (A, first last)) to ensure that an array does not have any repeated elements as well as check that two arrays of the same size and type represent the same set.
+Preconditions: N/A
+Postconditions: N/A
+*/
+
+#include<iostream>
+#include<cmath>
+#include<ctime>
+
+using namespace std;
+
+int main(){
+
+return 0;
+
+}

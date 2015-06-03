e43bdb371679ec0258a587bba31a1e67b5a7a3fc
snhendo <snhllama74@sbcglobal.net>
2015-06-02 22:16:37 -0400

    added bubblesort

--- HW2/main.cpp
+++ HW2/main.cpp
@@ -5,7 +5,12 @@ Course: Data Structures
 Course Number: CS2028
 Instructor: Anca Ralescu
 TA: Suryadip Chakraborty
-Abstract: Assignment 2 main.cpp uses three algorithms (Algorithm isUnique1 (A, first, last), Algorithm isUnique2 (A, first, last), and Algorithm isUnique3 (A, first last)) to ensure that an array does not have any repeated elements as well as check that two arrays of the same size and type represent the same set.
+Abstract: Assignment 2 main.cpp uses three algorithms:
+          Algorithm isUnique1(A, first, last)
+          Algorithm isUnique2 (A, first, last)
+          Algorithm isUnique3 (A, first last),
+          to ensure that an array does not have any repeated elements as well as
+          check that two arrays of the same size and type represent the same set.
 Preconditions: N/A
 Postconditions: N/A
 */
@@ -56,6 +61,26 @@ false if the array contains repeated elements*/
         if (A[i] == A[i+1]) {return false};
     }
    return true;
+}
+
+
+// bubble sort from talaga's examples
+void bubbleSort(vector<int> &v){
+  for(unsigned int runs = 0; runs < v.size(); runs++){
+    bool swapped = false;
+    for(unsigned int i = 0; i < v.size()-1; i++){
+      if(v[i] > v[i+1]){  // swap needed!
+        int temp = v[i];
+        v[i] = v[i+1];
+        v[i+1] = temp;
+        swapped = true;
+      }
+    }
+    if(!swapped){
+      //cout << "Out for good behavior\n";
+      return;
+    }
+  }
 }
 
 return 0;

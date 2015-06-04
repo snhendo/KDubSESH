9eca9e0705c815164dcbbf1b754083ebeaa0529e
snhendo <snhllama74@sbcglobal.net>
2015-06-04 19:50:32 -0400

    moved some stuff, added quicksort 😎

--- HW2/main.cpp
+++ HW2/main.cpp
@@ -6,13 +6,14 @@ Course Number: CS2028
 Instructor: Anca Ralescu
 TA: Suryadip Chakraborty
 Abstract: Assignment 2 main.cpp uses three algorithms:
-          Algorithm isUnique1(A, first, last)
-          Algorithm isUnique2 (A, first, last)
-          Algorithm isUnique3 (A, first, last),
-          to ensure that an array does not have any repeated elements as well as
-          check that two arrays of the same size and type represent the same set.
+		  Algorithm isUnique1(A, first, last)
+		  Algorithm isUnique2 (A, first, last)
+		  Algorithm isUnique3 (A, first, last),
+		  to ensure that an array does not have any repeated elements as well as
+		  check that two arrays of the same size and type represent the same set.
 Preconditions: N/A
 Postconditions: N/A
+Credit: Used examples from CS2/Dr. Paul Talaga
 */
 
 #include<iostream>
@@ -22,115 +23,157 @@ Postconditions: N/A
 
 using namespace std;
 
-int main(){
 
-bool isUnique1(vector<int> A, int first, int last){ //Recursive validation
-   /*Input: Array A, first, last
-   Output:
-true if the array contains no repeated elements
-false if the array contains repeated elements*/
-    if (first >= last) {return true};
-    if (!isUnique1(A, first, last-1) {return false};
-    if (!isUnique1(A, first+1, last) {return false};
-    return(A[first]!=A[last])
-}
-
-
-bool isUnique2(vector<int> A, int first, int last){ //Iterative validation
-   /*Input: Array A, first, last
-   Output:
-true if the array contains no repeated elements
-false if the array contains repeated elements*/
-    if (first >= last) {return true;}
-    for (int i =  first; i < last; i++) {
-        for (int j = i+1; j <=last; j++) {
-            if (A[i] == A[j]) {return false};
-        }
-    }
-   return true;
-}
-
-bool isUnique3(vector<int> A, int first, int last, int sortType){ //Sorted validation
-   /*Input: Array A, first, last
-   Output:
-true if the array contains no repeated elements
-false if the array contains repeated elements*/
-    if (sortType == 0) {
-        A = bubbleSort(A);
-    } else if (sortType == 1) {
-        A = quickSort(A);
-    } else {
-        A = selectionSort(A);
-    }
-    if (first >= last) {return true;}
-    for (int i =  first; i < last; i++) {
-        if (A[i] == A[i+1]) {return false};
-    }
-   return true;
-}
-
-
-// bubble sort from talaga's examples
-//i think we're supposed to use arrays not vectors?
-vector bubbleSort(vector<int> &v){
-  for(unsigned int runs = 0; runs < v.size(); runs++){
-    bool swapped = false;
-    for(unsigned int i = 0; i < v.size()-1; i++){
-      if(v[i] > v[i+1]){  // swap needed!
-        int temp = v[i];
-        v[i] = v[i+1];
-        v[i+1] = temp;
-        swapped = true;
-      }
-    }
-    if(!swapped){
-      //cout << "Out for good behavior\n";
-      return;
-    }
+void fill(vector<int> &numbers, int count){
+	int size = count;
+	while(count > 0){
+	numbers.push_back(rand() % (size*2));
+	count--;
   }
 }
 
-        time_t start = time;
-        time_t end = time;
-        bool result = true;
+// bubble sort from talaga's examples
+void bubbleSort(vector<int> &numbers){
+  for(unsigned int runs = 0; runs < numbers.size(); runs++){
+	bool swapped = false;
+	for(unsigned int i = 0; i < numbers.size()-1; i++){
+	  if(numbers[i] > numbers[i+1]){  //swap
+		int temp = numbers[i];
+		numbers[i] = numbers[i+1];
+		numbers[i+1] = temp;
+		swapped = true;
+	  }
+	}
+	if(!swapped){
+	  //cout << "Out for good behavior\n";
+	  return;
+	}
+  }
+}
 
-        vector<int> numbers;
+//talaga
+void quickSort(vector<int> &numbers, int first, int last){
+	// sort the elements between (and including) start and end
+	if(last-first < 1){ // 0
+		return;
+	}
+	int so = first;
+	int se = last;
+	// put pivot at start location
+	int pivot = first;
+	int pv = numbers[pivot];
+	while(last-first >= 1){
+		if(numbers[first] == pv && numbers[last] == pv){
+		  first++;
+		}else if(numbers[first] >= pv && numbers[last] <= pv){ // switch
+		  int temp = numbers[first];
+		  numbers[first] = numbers[last];
+		  numbers[last] = temp;
+		  // don't change start or end
+		}else if(numbers[last] > pv){
+		  last--;
+		}else if(numbers[first] < pv){ // low OK
+		  first++;
+		}else{
+		  cout << "Something went wrong\n";
+		  return;
+		}
+	}
+	quickSort(numbers,so, first-1);
+	quickSort(numbers,last+1,se);
+}
 
-        start = time;
-        result = isUnique1(numbers, 0, numbers.size());
-        end = time;
-        cout << "Recursive Validation :: ";
-        if (result == false) {
-             cout << "NOT ";
-        }
-        cout << "Unique! Time: " << ctime(difftime(end,start)) << endl;
 
-        start = time;
-        result = isUnique2(numbers, 0, numbers.size());
-        end = time;
-        cout << "Iterative Validation :: ";
-        if (result == false) {
-             cout << "NOT ";
-        }
-        cout << "Unique! Time: " << ctime(difftime(end,start)) << endl;
+bool isUnique1(vector<int> &numbers, int first, int last){ //Recursive validation
+   /*Input: Array A, first, last
+   Output:
+true if the array contains no repeated elements
+false if the array contains repeated elements*/
+	if (first >= last) {return true};
+	if (!isUnique1(numbers, first, last-1) {return false};
+	if (!isUnique1(numbers, first+1, last) {return false};
+	return(numbers[first]!=numbers[last])
+}
 
-        for (int sortType = 0; sortType <= 2; sortType++) {
-            start = time;
-            result = isUnique3(numbers, 0, numbers.size(), sortType);
-            end = time;
-            if (sortType == 0) {
-                cout << "BubbleSort ";
-            } else if (sortType == 1) {
-                cout << "QuickSort ";
-            } else {
-                cout << "SelectionSort ";
-            }
-            cout << "Sorted Validation :: ";
-            if (result == false) {
-                 cout << "NOT ";
-            }
-            cout << "Unique! Time: " << ctime(difftime(end,start)) << endl;
-        }
+
+bool isUnique2(vector<int> &numbers, int first, int last){ //Iterative validation
+   /*Input: Array A, first, last
+   Output:
+true if the array contains no repeated elements
+false if the array contains repeated elements*/
+	if (first >= last) {return true;}
+	for (int i =  first; i < last; i++) {
+		for (int j = i+1; j <=last; j++) {
+			if (numbers[i] == numbers[j]) {return false};
+		}
+	}
+   return true;
+}
+
+bool isUnique3(vector<int> numbers, int first, int last, int sortType){ //Sorted validation
+   /*Input: Array A, first, last
+   Output:
+true if the array contains no repeated elements
+false if the array contains repeated elements*/
+	if (sortType == 0) {
+		bubbleSort(numbers);
+	}
+	else if (sortType == 1) {
+		quickSort(numbers);
+	} else {
+		selectionSort(numbers);
+	} if (first >= last) {return true;}
+	for (int i =  first; i < last; i++) {
+		if (numbers[i] == numbers[i+1]) {return false};
+	}
+   return true;
+}
+
+
+int main(){
+	time_t start = time;
+	time_t end = time;
+	bool result = true;
+
+	vector<int> numbers;
+	fill(number, size);
+
+	start = time;
+	result = isUnique1(numbers, 0, numbers.size());
+	end = time;
+	cout << "Recursive Validation :: ";
+	if (result == false) {
+		 cout << "NOT ";
+	}
+	cout << "Unique! Time: " << ctime(difftime(end,start)) << endl;
+
+	start = time;
+	result = isUnique2(numbers, 0, numbers.size());
+	end = time;
+	cout << "Iterative Validation :: ";
+	if (result == false) {
+		 cout << "NOT ";
+	}
+	cout << "Unique! Time: " << ctime(difftime(end,start)) << endl;
+
+	for (int sortType = 0; sortType <= 2; sortType++) {
+		start = time;
+		result = isUnique3(numbers, 0, numbers.size(), sortType);
+		end = time;
+		if (sortType == 0) {
+			cout << "BubbleSort ";
+
+		} else if (sortType == 1) {
+			cout << "QuickSort ";
+		} else {
+			cout << "SelectionSort ";
+		}
+		cout << "Sorted Validation :: ";
+		if (result == false) {
+			 cout << "NOT ";
+		}
+		cout << "Unique! Time: " << ctime(difftime(end,start)) << endl;
+	}
 
 return 0;
 

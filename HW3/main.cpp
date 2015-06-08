cbf44d58615a04087d8d5acd1428421aab56ef4c
K Waters <waterskm@mail.uc.edu>
2015-06-08 15:06:15 -0400

    Created initial stuff for HW3
    folder and main.cpp

---
+++ HW3/main.cpp
@@ -0,0 +1,21 @@
+/* 20CS2028 Data Structures Assignment #3 - Due June 17, 2015
+Author: Stephen Enochian, Shannon Henderson, Kaitlyn Waters
+Course title: Data Structures
+Course number: CS2028
+Instructor: Anca Ralescu
+TA: Suryadip Chakraborty
+Abstract: Assignment 3 main.cpp uses doubly-linked lists to imitate an email inbox by using functions <insert function names here> to do <the following tasks>.
+Preconditions: None
+Postconditions: None
+Credit: Referred to Dr. Talaga's examples from CS2
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+return 0;
+
+}

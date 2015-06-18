a31a783ca83f30960dbc48c6a1e4c45a80200c7a
Stephen Enochian <enochisg@mail.uc.edu>
2015-06-18 18:22:27 -0400

    We changed :poop:

--- HW3/main.cpp
+++ HW3/main.cpp
@@ -14,6 +14,92 @@ Credit: Referred to Dr. Talaga's examples from CS2
 
 using namespace std;
 
+class Email(){
+    public:
+
+    Email();
+    ~Email();
+
+    private:
+
+    class emailNode{
+        public:
+        string to;
+        string from;
+        string message;
+    }
+    emailNode* head;
+}
+
+Email::Email(){
+    head = NULL;
+}
+
+Email::~Email();
+
+class Inbox(){
+    public:
+
+    Inbox();
+    ~Inbox();
+    void insertEmail();
+    void deleteCommunication();
+    void displayInbox();
+
+    private:
+
+    class commNode{
+        public:
+        string subject;
+        int emailCount;
+        commNode* next;
+        commNode* previous;
+    }
+    commNode* head;
+    int size;
+    commNode* searchCommunication();
+}
+
+Inbox::Inbox(){
+    head = NULL;
+    size = 0;
+}
+
+Inbox::~Inbox();
+
+commNode* Inbox::searchCommunication(string newSubject) const{
+    commNode* temp = head;
+    while (temp != NULL){
+        if(temp->subject == newSubject){
+            return temp;
+        }
+        if(temp->next != NULL){
+            temp = temp->next;
+        }else{
+            return NULL;
+        }
+    }
+}
+
+void Inbox::displayInbox() const{
+    commNode* temp = head;
+    while(temp != NULL){
+        cout << temp->subject << " - " << temp->emailCount << endl;
+        if(temp->next != NULL){
+            temp = temp->next;
+        }else{
+            temp = NULL;
+        }
+    }
+}
+
+void Inbox::insertEmail(string newSubject; string to; string from; string message) const{
+    commNode* temp = searchCommunication(newSubject);
+    if(temp != NULL){
+
+    }
+}
+
 int main() {
 
 return 0;

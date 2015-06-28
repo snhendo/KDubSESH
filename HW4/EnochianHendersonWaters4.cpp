3c30fb516efe09ff9746f780f5751fc7a07eaf8c
Stephen Enochian <enochisg@mail.uc.edu>
2015-06-28 15:12:41 -0400

    Stuffffffffssss happeneddededed

---
+++ HW4/EnochianHendersonWaters4.cpp
@@ -0,0 +1,12 @@
+/* 20CS2028 Data Structures Assignment #4 - Due July 7, 2015
+Author: Stephen Enochian, Shannon Henderson, Kaitlyn Waters
+Course title: Data Structures
+Course number: CS2028
+Instructor: Anca Ralescu
+TA: Suryadip Chakraborty
+Abstract: This program prompts for information and then sorts the given list given its size using MergeSort, QuickSort, BubbleSort, and InsertionSort via a HybridSort function.
+Preconditions: None
+Postconditions: None
+Credit: Referred to Dr. Talaga's examples from CS2
+*/
+

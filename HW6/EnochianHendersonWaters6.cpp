e8d0dda60734b9f3108dbf09fd02363d7fd097b6
snhendo <snhllama74@sbcglobal.net>
2015-07-28 19:55:46 -0400

    added hash things

--- HW6/EnochianHendersonWaters6.cpp
+++ HW6/EnochianHendersonWaters6.cpp
@@ -14,19 +14,85 @@ Credit: Referred to Dr. Talaga's examples from CS2, consulted the web for variou
 #include <string>
 #include <vector>
 #include <stdlib.h>
+#include <cmath>
 
 using namespace std;
+
+
+class HashEntry{
+    private:
+        int key;
+        int value;
+    public:
+        HashEntry(int key, int value){
+            this->key = key;
+            this->value = value;
+        }
+
+        int getKey(){ return key; }
+        int getValue(){ return value; }
+};
+
+//DECLARE TABLE SIZE
+const int N = 23;
+
+class HashTable{
+    private:
+    HashEntry** table;
+
+    public:
+    HashTable(){
+        table = new HashEntry*[N];
+        for(int i = 0; i < N; i++){
+            table[i] = NULL;
+        }
+    }
+
+    int get(int key){
+        int hash = (key %  N);
+        while(table[hash] != NULL && table[hash]->getKey() != key){
+            hash = (hash + 1) % N;
+        }
+        if(table[hash] == NULL){
+            return -1;
+        }
+        else{
+            return table[hash]->getValue();
+        }
+    }
+
+    void put(int key, int value){
+        int hash = (key % N);
+        while(table[hash] != NULL && table[hash]->getKey() != key){
+            hash = (hash + 1) % N;
+        }
+        if (table[hash] != NULL){
+            delete table[hash];
+        }
+        table[hash] = new HashEntry(key, value);
+    }
+
+    ~HashTable(){
+        for(int i = 0; i < N; i++){
+            if(table[i] != NULL){
+                delete table[i];
+            }
+        }
+        delete[] table;
+    }
+};
+
 
 int main () {
     cout << "Assignment #6" << endl << endl;
     cout << "Select your collision resolution scheme:\n(a) Double Hashing\n(b) Quadritic\n(c) Chaining within the Hash Table\n";
-    char collisionResolutionSelection = '';
+    char* collisionResolutionSelection;
     cin >> collisionResolutionSelection;
-    if (collisionResolutionSelection < 'a' || collisionResolutionSelection > 'c') {
+    if (*collisionResolutionSelection < 'a' || *collisionResolutionSelection > 'c') {
         return 0;
     }
-    cout << "What load ratio would you like to use? Enter a value between 0 and 1.0" endl;
+    cout << "What load ratio would you like to use? Enter a value between 0 and 1.0" << endl;
     float loadRatio = 0.0;
-    cin >> loadRadio;
+    cin >> loadRatio;
     return 0;
 }
